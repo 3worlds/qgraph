ec1620964f183c0d88bde8c591ad6329223a2d85
Jacques Gignoux <jacques.gignoux@upmc.fr>
2019-09-04 12:46:04 +0200

    fixing dependency versions

--- qgraph/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ qgraph/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -55,8 +55,8 @@ public class VersionSettings {
 	 * 
 	 */
 	protected static String[][] DEPS = { 
-		{"fr.ens.biologie", "generics", "[0.1.0,)", null},
-		{"au.edu.anu.rscs.aot", "omugi", "[0.2.1,)", null},
+		{"fr.ens.biologie", "generics", "[0.1.10,)", null},
+		{"au.edu.anu.rscs.aot", "omugi", "[0.2.10,)", null},
 	};
 	
 	/** The name of the main class to put in the jar manifest, if any. This enables users to
