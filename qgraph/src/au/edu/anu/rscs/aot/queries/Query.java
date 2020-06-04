31cf5f865872fb40798a72d9040d5fcbd8ae67ef
Ian Davies <ian.davies@anu.edu.au>
2020-06-05 09:27:52 +1000

    Use Element.toShortString() for queries

--- qgraph/src/au/edu/anu/rscs/aot/queries/Query.java
+++ qgraph/src/au/edu/anu/rscs/aot/queries/Query.java
@@ -32,27 +32,44 @@ package au.edu.anu.rscs.aot.queries;
 import java.util.logging.Logger;
 
 import au.edu.anu.rscs.aot.QGraphException;
+import fr.cnrs.iees.graph.Element;
 import fr.ens.biologie.generic.utils.Logging;
 
 /**
- * <p>The ancestor class for the Query system.</p>
+ * <p>
+ * The ancestor class for the Query system.
+ * </p>
  * <h3>Usage:</h3>
- * <p>All Queries work in the same way: </p>
+ * <p>
+ * All Queries work in the same way:
+ * </p>
  * <ul>
- * <li>A subclass of Query, matching the particular condition to fulfill, is constructed with some parameters;</li>
- * <li>Later on, a call to {@code Query.satisfied(Object)} will check the condition
- * and return true if the Query was satistfied;</li>
- * <li>Alternatively, a call to {@code Query.check(Object)} can be made, that will return
- * an Exception if the query is not satisfied.</li>
+ * <li>A subclass of Query, matching the particular condition to fulfill, is
+ * constructed with some parameters;</li>
+ * <li>Later on, a call to {@code Query.satisfied(Object)} will check the
+ * condition and return true if the Query was satistfied;</li>
+ * <li>Alternatively, a call to {@code Query.check(Object)} can be made, that
+ * will return an Exception if the query is not satisfied.</li>
  * </ul>
- * <p>Most Query subclasses come with static methods implementing the most common use cases for their class.</p>
- * <p>Queries in the base package apply to non-graph objects.</p>
- * <p>The {@code process()} method parameter type is generic ({@link Object}), but for every 
- * particular Query a specific type is required (cf. the javadoc for each specific Query type).</p>
- * <p>Some queries compute a result, accessible through the {@code getResult()} method. The 
- * default return type is {@code Object}, but the actual return type has to be known (cf. the
- * javadoc for each specific Query type). A typical use case is returning a filtered list
- * when the input to {@code process()} is a list of items.</p>
+ * <p>
+ * Most Query subclasses come with static methods implementing the most common
+ * use cases for their class.
+ * </p>
+ * <p>
+ * Queries in the base package apply to non-graph objects.
+ * </p>
+ * <p>
+ * The {@code process()} method parameter type is generic ({@link Object}), but
+ * for every particular Query a specific type is required (cf. the javadoc for
+ * each specific Query type).
+ * </p>
+ * <p>
+ * Some queries compute a result, accessible through the {@code getResult()}
+ * method. The default return type is {@code Object}, but the actual return type
+ * has to be known (cf. the javadoc for each specific Query type). A typical use
+ * case is returning a filtered list when the input to {@code process()} is a
+ * list of items.
+ * </p>
  * 
  * @author Shayne Flint - 15/3/2012
  *
@@ -60,7 +77,7 @@ import fr.ens.biologie.generic.utils.Logging;
 public abstract class Query {
 
 	private static Logger log = Logging.getLogger(Query.class);
-	protected Object  result    = null;
+	protected Object result = null;
 	protected boolean satisfied = false;
 
 	/**
@@ -80,7 +97,8 @@ public abstract class Query {
 	}
 
 	/**
-	 * Processes the Query constraint on the input 
+	 * Processes the Query constraint on the input
+	 * 
 	 * @param input the object to check
 	 * @return this Query (for agile programming)
 	 */
@@ -88,11 +106,12 @@ public abstract class Query {
 
 	/**
 	 * Default processing, always returning satisfied = false and result = input.
+	 * 
 	 * @param input the object to check
 	 * @return this Query (for agile programming)
 	 */
 	protected Query defaultProcess(Object input) {
-		//		this.input = input;
+		// this.input = input;
 		result = input;
 		satisfied = false;
 		return this;
@@ -100,6 +119,7 @@ public abstract class Query {
 
 	/**
 	 * Calls {@code process(input)} and returns true if satisfied
+	 * 
 	 * @param input the object to check
 	 * @return true if the Query was satisfied
 	 */
@@ -108,59 +128,66 @@ public abstract class Query {
 		return satisfied;
 	}
 
-
 	/**
 	 * Calls {@code process(input)} and throws an Exception if not satisfied
+	 * 
 	 * @param input the object to check
 	 */
 	public void check(Object item) {
 		process(item);
 		if (!satisfied()) {
-			//			Log.log("Query.check failed - " + toString());
+			// Log.log("Query.check failed - " + toString());
 			failed(item);
 		}
 	}
 
-
 	// Constraint failure messages
 	//
 
+	private static String itemString(Object item) {
+		if (item instanceof Element) {
+			return ((Element) item).toShortString();
+		}
+		return item.toString();
+	}
+
 	protected void failed(Object item) {
-		String message = "'" + item.toString() + "' does not satisfy '" + toString() + "'";
-		//		Log.log("Query: " + message);
+
+		String message = "'" + itemString(item) + "' does not satisfy '" + toString() + "'";
+		// Log.log("Query: " + message);
 		throw new QGraphException(message);
 	}
 
 	protected void failed(Object item, Exception e) {
-		String message = "'" + item.toString() + "' does not satisfy '" + toString() + "' [" + e + "]";
-		//		Log.log("Query: " + message);
+		String message = "'" + itemString(item) + "' does not satisfy '" + toString() + "' [" + e + "]";
+		// Log.log("Query: " + message);
 		throw new QGraphException(message);
 	}
 
 	protected void failed(Object item, String msg) {
-		String message = "'" + item.toString() + "' does not satisfy '" + toString() + "' [" + msg + "]";
-		//		Log.log("Query: " + message);
+		String message = "'" + itemString(item) + "' does not satisfy '" + toString() + "' [" + msg + "]";
+		// Log.log("Query: " + message);
 		throw new QGraphException(message);
 	}
-
 
 	// Representation
 	//
 
 	public static String queryFailedStr = "X!";
+
 	public String stateString() {
 		if (satisfied)
-			return ""; //"[Passed on " + input + "]";
+			return ""; // "[Passed on " + input + "]";
 		else
-			return queryFailedStr; //"[Failed on " + input + "]";
+			return queryFailedStr; // "[Failed on " + input + "]";
 	}
 
 	public String toString() {
-		return "[" + this.getClass().getName() + ", satisfied=" + satisfied + ", result=[" + result + "]]";
+		return "[" + this.getClass().getSimpleName() + ", satisfied=" + satisfied + ", result=[" + result + "]]";
 	}
 
 	public String userString() {
-		return "[" + stateString() + this.getClass().getName() + "]";
+		return "[" + stateString() + this.getClass().getSimpleName() + "]";
 	}
 
 	/**
@@ -169,7 +196,6 @@ public abstract class Query {
 	public void log() {
 		log.info(toString());
 	}
-
 
 	// Trace
 	//
@@ -181,7 +207,7 @@ public abstract class Query {
 		else
 			System.out.println("X " + indent + query);
 		if (query instanceof QueryList) {
-			for (Query q : ((QueryList)query).queryList())
+			for (Query q : ((QueryList) query).queryList())
 				trace("  ", q, item);
 		}
 	}
