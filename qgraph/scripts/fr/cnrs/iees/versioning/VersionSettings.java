7714d6499b8f0f37aa73fc52d37d8503a4dd790a
Jacques Gignoux <jacques.gignoux@upmc.fr>
2022-08-24 14:45:48 +0200

    dependency consistency

--- qgraph/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ qgraph/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -84,7 +84,7 @@ public class VersionSettings {
 	 *
 	 */
 	protected static String[][] DEPS = {
-		{"fr.ens.biologie", "generics", "[0.3.1,)", null},
+		{"fr.ens.biologie", "generics", "[0.4.0,)", null},
 		{"au.edu.anu.rscs.aot", "omugi", "[0.5.0,)", null},
 	};
 
